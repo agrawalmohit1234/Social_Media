b310bb725abed4a9a4a4931c7fe433ca2b50f45e
unknown <rajagrawalmohit2@gmail.com>
2021-04-27 14:01:23 +0530

    Now users upload a profile photo and add a short bio description to their profiles.

--- server/server/controllers/user.controller.js
+++ server/server/controllers/user.controller.js
@@ -1,87 +1,120 @@
-const User = require('../models/user.model')
-const extend = require('lodash/extend')
-const errorHandler = require('./../helpers/dbErrorHandler')
+const formidable = require('formidable');
+const fs = require('fs');
+const path = require('path');
+const profileImage = './../assets/images/profile.png';
+const User = require('../models/user.model');
+const extend = require('lodash/extend');
+const errorHandler = require('./../helpers/dbErrorHandler');
 
 const create = async (req, res) => {
-    try {
-        const user = new User(req.body)
-        await user.save()
-        return res.status(201).json({
-            message: "Successfully signed up!"
-        })
-    } catch (err) {
-        return res.status(400).json({
-            error: errorHandler.getErrorMessage(err)
-        })
-    }
-}
+  try {
+    const user = new User(req.body);
+    await user.save();
+    return res.status(201).json({
+      message: 'Successfully signed up!',
+    });
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err),
+    });
+  }
+};
 
 const list = async (req, res) => {
-    try {
-        const users = await User.find().select('name email updated created')
-        res.status(200).json(users)
-    } catch (err) {
-        return res.status(400).json({
-            error: errorHandler.getErrorMessage(err)
-        })
-    }
-}
-
+  try {
+    const users = await User.find().select('name email updated created');
+    res.status(200).json(users);
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err),
+    });
+  }
+};
 
 const userByID = async (req, res, next, id) => {
-    try {
-        let user = await User.findById(id)
-        if (!user)
-            return res.status('400').json({
-                error: "User not found"
-            })
-        req.profile = user
-        next()
-    } catch (err) {
-        return res.status('400').json({
-            error: "Could not retrieve user"
-        })
-    }
-}
-
+  try {
+    let user = await User.findById(id);
+    if (!user)
+      return res.status('400').json({
+        error: 'User not found',
+      });
+    req.profile = user;
+    next();
+  } catch (err) {
+    return res.status('400').json({
+      error: 'Could not retrieve user',
+    });
+  }
+};
 
 const read = (req, res) => {
-    req.profile.hashed_password = undefined
-    req.profile.salt = undefined
-    return res.json(req.profile)
-}
-
+  req.profile.hashed_password = undefined;
+  req.profile.salt = undefined;
+  return res.json(req.profile);
+};
 
 const update = async (req, res) => {
-    try {
-        let user = req.profile
-        user = extend(user, req.body)
-        user.updated = Date.now()
-        await user.save()
-        user.hashed_password = undefined
-        user.salt = undefined
-        res.json(user)
-    } catch (err) {
-        return res.status(400).json({
-            error: errorHandler.getErrorMessage(err)
-        })
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, async (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: 'Photo could not be uploaded',
+      });
     }
-}
-
+    let user = req.profile;
+    user = extend(user, fields);
+    user.updated = Date.now();
+    if (files.photo) {
+      user.photo.data = fs.readFileSync(files.photo.path);
+      user.photo.contentType = files.photo.type;
+    }
+    try {
+      await user.save();
+      user.hashed_password = undefined;
+      user.salt = undefined;
+      res.json(user);
+    } catch (err) {
+      return res.status(400).json({
+        error: errorHandler.getErrorMessage(err),
+      });
+    }
+  });
+};
 
 const remove = async (req, res) => {
-    try {
-        let user = req.profile
-        let deletedUser = await user.remove()
-        deletedUser.hashed_password = undefined
-        deletedUser.salt = undefined
-        res.json(deletedUser)
-    } catch (err) {
-        return res.status(400).json({
-            error: errorHandler.getErrorMessage(err)
-        })
-    }
-}
+  try {
+    let user = req.profile;
+    let deletedUser = await user.remove();
+    deletedUser.hashed_password = undefined;
+    deletedUser.salt = undefined;
+    res.json(deletedUser);
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err),
+    });
+  }
+};
 
+const photo = (req, res, next) => {
+  if (req.profile.photo.data) {
+    res.set('Content-Type', req.profile.photo.contentType);
+    return res.send(req.profile.photo.data);
+  }
+  next();
+};
 
-module.exports = { create, userByID, read, list, remove, update }
+const defaultPhoto = (req, res) => {
+  return res.sendFile(path.join(__dirname, './../assets/images/profile.png'));
+};
+
+module.exports = {
+  create,
+  userByID,
+  read,
+  list,
+  remove,
+  update,
+  photo,
+  defaultPhoto,
+};
--- server/server/routes/user.routes.js
+++ server/server/routes/user.routes.js
@@ -1,15 +1,18 @@
-const express = require('express')
-const userCtrl = require('../controllers/user.controller')
-const authCtrl = require('../controllers/auth.controller')
-const router = express.Router()
-router.route('/api/users')
-    .get(userCtrl.list)
-    .post(userCtrl.create)
-router.route('/api/users/:userId')
-    .get(authCtrl.requireSignin, userCtrl.read)
-    .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
-    .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove)
+const express = require('express');
+const userCtrl = require('../controllers/user.controller');
+const authCtrl = require('../controllers/auth.controller');
+const router = express.Router();
+router.route('/api/users').get(userCtrl.list).post(userCtrl.create);
+router
+  .route('/api/users/photo/:userId')
+  .get(userCtrl.photo, userCtrl.defaultPhoto);
+router.route('/api/users/defaultphoto').get(userCtrl.defaultPhoto);
+router
+  .route('/api/users/:userId')
+  .get(authCtrl.requireSignin, userCtrl.read)
+  .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
+  .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove);
 
-router.param('userId', userCtrl.userByID)
+router.param('userId', userCtrl.userByID);
 
-module.exports = router
+module.exports = router;
